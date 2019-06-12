158c1eea7cf3a809f998692992430a98abae6696
chenxi <18603854251@wo.cn>
2019-06-12 11:16:29 +0800

    change jacob to chen, change ethan to xi

--- src/fxmltableview/FXMLTableViewController.java
+++ src/fxmltableview/FXMLTableViewController.java
@@ -55,8 +55,8 @@ public class FXMLTableViewController implements Initializable {
         fName.setCellValueFactory(new PropertyValueFactory("firstName"));
         lName.setCellValueFactory(new PropertyValueFactory("lastName"));
         emailValue.setCellValueFactory(new PropertyValueFactory("email"));
-        Person jacob = new Person("Jacob", "Smithy", "jacob.smithy@example.com");
-        Person ethan = new Person("Ethan", "William", "ethan.william@example.com");
+        Person jacob = new Person("chen", "Smithy", "jacob.smithy@example.com");
+        Person ethan = new Person("xi", "William", "ethan.william@example.com");
         list.add(jacob);
         list.add(ethan);
         tableView.setItems(list);
